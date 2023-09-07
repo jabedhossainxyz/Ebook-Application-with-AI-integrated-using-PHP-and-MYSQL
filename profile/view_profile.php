9661602d3bb0e1cb0b3ca3c3a3c28e7b5e7b94f9
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-09-07 22:25:07 +0600

    07 september 2023

--- profile/view_profile.php
+++ profile/view_profile.php
@@ -39,6 +39,7 @@ $conn->close();
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
         <title>Profile</title>
 
@@ -72,7 +73,11 @@ $conn->close();
                 <p><strong>Username:</strong> <?php echo $user['username']; ?></p>
                 <p><strong>Email:</strong> <?php echo $user['email']; ?></p>
                 <p><strong>Mobile:</strong> <?php echo $user['mobile']; ?></p>
-                <a class="btn btn-primary" href="../views/dashboard.php">Back to Dashboard</a>
+                <div class="w-50">
+                        <a class="btn btn-primary" href="../views/dashboard.php">
+                                <i class="fas fa-arrow-left"></i> Back to Dashboard
+                        </a>
+                </div>
         </div>
 
 </body>
